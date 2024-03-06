1d4c95c91ec9fd40590fe91ac1c7b97bcae03d47
Vishal Vaddadhi <vishalvaddadhi@Vishals-MacBook-Pro.local>
2024-03-05 22:26:17 -0800

    formatting code

--- backend/routes/newsRoutes.js
+++ backend/routes/newsRoutes.js
@@ -1,50 +1,58 @@
-const express = require('express');
-const axios = require('axios');
-const summarizeText = require('../utils/summarize');
+const express = require("express");
+const axios = require("axios");
+const summarizeText = require("../utils/summarize");
 const router = express.Router();
 
-router.get('/news', async (req, res) => {
+router.get("/news", async (req, res) => {
   const { query } = req.query;
   try {
     // Fetch articles from the API
-    const response = await axios.get(`https://newsapi.org/v2/everything?q=${encodeURIComponent(query)}&apiKey=${process.env.NEWSAPI_KEY}&language=en`);
+    const response = await axios.get(
+      `https://newsapi.org/v2/everything?q=${encodeURIComponent(
+        query
+      )}&apiKey=${process.env.NEWSAPI_KEY}&language=en`
+    );
     const maxLength = 100; // Set the maximum length of the description
     // Map articles to include only necessary info and sort them from newest to oldest
     const articles = response.data.articles
-    .map(article => ({
-      source: article.source.name,
-      title: article.title,
-      description: article.description && article.description.length > maxLength 
-                    ? article.description.substring(0, maxLength) + '...' 
-                    : article.description,
-      url: article.url,
-      content: article.content,
-      publishedAt: article.publishedAt,
-      formattedDate: new Date(article.publishedAt).toLocaleDateString('en-US', {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric'
-      }) // Adding formatted date
-    }))
-    .sort((a, b) => new Date(b.publishedAt) - new Date(a.publishedAt));
+      .map((article) => ({
+        source: article.source.name,
+        title: article.title,
+        description:
+          article.description && article.description.length > maxLength
+            ? article.description.substring(0, maxLength) + "..."
+            : article.description,
+        url: article.url,
+        content: article.content,
+        publishedAt: article.publishedAt,
+        formattedDate: new Date(article.publishedAt).toLocaleDateString(
+          "en-US",
+          {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          }
+        ), // Adding formatted date
+      }))
+      .sort((a, b) => new Date(b.publishedAt) - new Date(a.publishedAt));
 
     // Send the sorted articles to the frontend
     res.json(articles);
   } catch (error) {
     console.error("Error fetching news:", error);
-    res.status(500).json({ message: 'Error fetching news' });
+    res.status(500).json({ message: "Error fetching news" });
   }
 });
 
-router.post('/summarize', async (req, res) => {
+router.post("/summarize", async (req, res) => {
   const { content } = req.body;
-  console.log('Received content for summarization:', req.body.content); // Debug log
+  console.log("Received content for summarization:", req.body.content); // Debug log
   try {
     const summary = await summarizeText(content);
     res.json({ summary });
   } catch (error) {
     console.error("Error summarizing article:", error);
-    res.status(500).json({ message: 'Error summarizing article' });
+    res.status(500).json({ message: "Error summarizing article" });
   }
 });
 
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,18 @@
-const express = require('express');
-require('dotenv').config(); // Load environment variables from .env file
+const express = require("express");
+require("dotenv").config(); // Load environment variables from .env file
 const app = express();
-const cors = require('cors');
-const newsRoutes = require('./routes/newsRoutes');
+const cors = require("cors");
+const newsRoutes = require("./routes/newsRoutes");
 
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(cors());
 
 // Test route
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.use('/api', newsRoutes);
+app.use("/api", newsRoutes);
 
 const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => {
