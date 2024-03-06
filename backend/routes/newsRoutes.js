f52929c5734a7fe4641fdd0cc9d69353a2ae7637
Vishal Vaddadhi <vishalvaddadhi@Vishals-MacBook-Pro.local>
2024-03-05 22:15:06 -0800

    style enhancements

--- backend/routes/newsRoutes.js
+++ backend/routes/newsRoutes.js
@@ -6,20 +6,35 @@ const router = express.Router();
 router.get('/news', async (req, res) => {
   const { query } = req.query;
   try {
-    const response = await axios.get(`https://newsapi.org/v2/everything?q=${encodeURIComponent(query)}&apiKey=${process.env.NEWSAPI_KEY}`);
-    const articles = response.data.articles.map(article => ({
+    // Fetch articles from the API
+    const response = await axios.get(`https://newsapi.org/v2/everything?q=${encodeURIComponent(query)}&apiKey=${process.env.NEWSAPI_KEY}&language=en`);
+    const maxLength = 100; // Set the maximum length of the description
+    // Map articles to include only necessary info and sort them from newest to oldest
+    const articles = response.data.articles
+    .map(article => ({
       source: article.source.name,
       title: article.title,
-      description: article.description,
+      description: article.description && article.description.length > maxLength 
+                    ? article.description.substring(0, maxLength) + '...' 
+                    : article.description,
       url: article.url,
-      content: article.content // Consider what you'll need for summarization
-    }));
+      content: article.content,
+      publishedAt: article.publishedAt,
+      formattedDate: new Date(article.publishedAt).toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+      }) // Adding formatted date
+    }))
+    .sort((a, b) => new Date(b.publishedAt) - new Date(a.publishedAt));
+
+    // Send the sorted articles to the frontend
     res.json(articles);
   } catch (error) {
+    console.error("Error fetching news:", error);
     res.status(500).json({ message: 'Error fetching news' });
   }
 });
-
 
 router.post('/summarize', async (req, res) => {
   const { content } = req.body;
